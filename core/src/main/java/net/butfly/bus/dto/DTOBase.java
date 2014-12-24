502b6ccbe1bbc75fd1e1751ec955a9d4e1380436
कलविङ्क <zhangx@hzcominfo.com>
2014-12-24 16:28:07 +0000

    refactor the deep copy and value cast utils.

--- core/src/main/java/net/butfly/bus/dto/DTOBase.java
+++ core/src/main/java/net/butfly/bus/dto/DTOBase.java
@@ -1,8 +1,8 @@
 package net.butfly.bus.dto;
 
-import net.butfly.albacore.support.BaseObjectSupport;
-import net.butfly.albacore.support.ObjectSupport;
+import net.butfly.albacore.support.BasicBean;
+import net.butfly.albacore.support.Beanable;
 
-public abstract class DTOBase<T extends ObjectSupport<T>> extends BaseObjectSupport<T> implements ObjectSupport<T> {
+public abstract class DTOBase<T extends Beanable<T>> extends BasicBean<T> implements Beanable<T> {
 	private static final long serialVersionUID = -2750322697249262868L;
 }
