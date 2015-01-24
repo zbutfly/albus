2e652e7639128a856eb40de8f51e0c0a8938763e
कलविङ्क <zhangx@hzcominfo.com>
2015-01-24 19:37:44 +0000

    add continuous supportting in Tasks, use subscribe to testing it.

--- core/src/main/java/net/butfly/bus/filter/FilterChain.java
+++ core/src/main/java/net/butfly/bus/filter/FilterChain.java
@@ -42,7 +42,7 @@ public final class FilterChain {
 				executeOne(filters[0], context);
 				return context.response();
 			}
-		}, context.callback(), context.invoker().localOptions(context.options())) {}.execute();
+		}, context.callback(), context.invoker().localOptions(context.options())).execute();
 	}
 
 	private void executeOne(final Filter filter, final FilterContext context) throws Exception {
