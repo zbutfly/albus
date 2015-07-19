8dd24ad238a3750600096f11eb81f88b9c376ef2
कलविङ्क <zhangx@hzcominfo.com>
2015-07-19 19:41:50 +0000

    enable scripts

--- pom.sh
+++ pom.sh
@@ -1,1 +1,3 @@
-mvn deploy:deploy-file -DpomFile=bus-node.pom -Dfile=bus-node.pom -Durl=http://repos.corp.butfly.co:60080/nexus/content/repositories/releases/
+#!/bin/bash
+
+mvn deploy:deploy-file -f node.pom -DpomFile=node.pom -Dfile=node.pom -Durl=http://repos.corp.butfly.co:60080/nexus/content/repositories/releases/
