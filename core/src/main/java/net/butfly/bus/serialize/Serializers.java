37c9555878e893577c33dca9da1693fc46621d52
कलविङ्क <zhangx@hzcominfo.com>
2016-03-12 16:53:25 +0000

    use lambda

--- core/src/main/java/net/butfly/bus/serialize/Serializers.java
+++ core/src/main/java/net/butfly/bus/serialize/Serializers.java
@@ -10,7 +10,6 @@ import com.google.common.base.Charsets;
 import net.butfly.albacore.utils.Instances;
 import net.butfly.albacore.utils.Reflections;
 import net.butfly.albacore.utils.Utils;
-import net.butfly.albacore.utils.async.Task;
 
 public final class Serializers extends Utils {
 	public static final String DEFAULT_MIME_TYPE = "text/plain";
@@ -18,18 +17,15 @@ public final class Serializers extends Utils {
 	public static final Class<? extends Serializer> DEFAULT_SERIALIZER_CLASS = scanDefaultSerializer();
 
 	public static Class<? extends Serializer> serializerClass(String mimeType) {
-		return Instances.fetch(new Task.Callable<Map<String, Class<? extends Serializer>>>() {
-			@Override
-			public Map<String, Class<? extends Serializer>> call() {
-				Map<String, Class<? extends Serializer>> map = new HashMap<String, Class<? extends Serializer>>();
-				for (Class<? extends Serializer> subClass : Reflections.getSubClasses(Serializer.class)) {
-					if (Modifier.isAbstract(subClass.getModifiers())) continue;
-					Serializer def = Instances.fetch(subClass, Serializers.DEFAULT_CHARSET);
-					for (String mime : def.supportedMimeTypes())
-						map.put(mime, subClass);
-				}
-				return map;
+		return Instances.fetch(() -> {
+			Map<String, Class<? extends Serializer>> map = new HashMap<String, Class<? extends Serializer>>();
+			for (Class<? extends Serializer> subClass : Reflections.getSubClasses(Serializer.class)) {
+				if (Modifier.isAbstract(subClass.getModifiers())) continue;
+				Serializer def = Instances.fetch(subClass, Serializers.DEFAULT_CHARSET);
+				for (String mime : def.supportedMimeTypes())
+					map.put(mime, subClass);
 			}
+			return map;
 		}).get(mimeType);
 	}
 
@@ -41,7 +37,8 @@ public final class Serializers extends Utils {
 		for (Class<? extends Serializer> cl : Reflections.getSubClasses(Serializer.class)) {
 			if (Modifier.isAbstract(cl.getModifiers()) || cl.getName().equals("net.butfly.bus.serialize.JSONSerializer")
 					|| cl.getName().equals("net.butfly.bus.serialize.HessianSerializer")
-					|| cl.getName().equals("net.butfly.bus.serialize.BurlapSerializer")) continue;
+					|| cl.getName().equals("net.butfly.bus.serialize.BurlapSerializer"))
+				continue;
 			return cl;
 		}
 		Class<? extends Serializer> cl = Reflections.forClassName("net.butfly.bus.serialize.HessianSerializer");
