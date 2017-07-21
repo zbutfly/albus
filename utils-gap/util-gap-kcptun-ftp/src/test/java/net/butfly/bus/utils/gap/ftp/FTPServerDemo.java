2518dcd976d2ce14187c7c7760237d3442564d41
taideli <1007811421@qq.com>
2017-07-21 14:58:40 +0800

    code format

--- utils-gap/util-gap-kcptun-ftp/src/test/java/net/butfly/bus/utils/gap/ftp/FTPServerDemo.java
+++ utils-gap/util-gap-kcptun-ftp/src/test/java/net/butfly/bus/utils/gap/ftp/FTPServerDemo.java
@@ -44,7 +44,6 @@ public class FTPServerDemo {
 
         factoryFactory.addCommand("STOR", (session, context, request) -> {
             try {
-
                 // get state variable
                 long skipLen = session.getFileOffset();
                 if (0L < skipLen) LOG.info("STOR offset [" + skipLen + "] will ignore.");
@@ -53,8 +52,8 @@ public class FTPServerDemo {
                 String fileName = request.getArgument();
                 if (fileName == null) {
                     session.write(LocalizedDataTransferFtpReply.translate(session, request, context,
-                            FtpReply.REPLY_501_SYNTAX_ERROR_IN_PARAMETERS_OR_ARGUMENTS,
-                            "STOR", null, null));
+                        FtpReply.REPLY_501_SYNTAX_ERROR_IN_PARAMETERS_OR_ARGUMENTS,
+                        "STOR", null, null));
                     return;
                 }
 
@@ -62,48 +61,18 @@ public class FTPServerDemo {
                 // https://issues.apache.org/jira/browse/FTPSERVER-110
                 DataConnectionFactory connFactory = session.getDataConnection();
                 if (connFactory instanceof IODataConnectionFactory) {
-                    InetAddress address = ((IODataConnectionFactory) connFactory)
-                            .getInetAddress();
+                    InetAddress address = ((IODataConnectionFactory) connFactory).getInetAddress();
                     if (address == null) {
                         session.write(new DefaultFtpReply(
-                                FtpReply.REPLY_503_BAD_SEQUENCE_OF_COMMANDS,
-                                "PORT or PASV must be issued first"));
+                            FtpReply.REPLY_503_BAD_SEQUENCE_OF_COMMANDS, "PORT or PASV must be issued first"));
                         return;
                     }
                 }
 
-                // 不需要获取文件及其权限了。
-                // get filename
-                FtpFile file = null;
-                /*try {
-                    file = session.getFileSystemView().getFile(fileName);
-                } catch (Exception ex) {
-                    LOG.debug("Exception getting file object", ex);
-                }
-                if (file == null) {
-                    session.write(LocalizedDataTransferFtpReply.translate(session, request, context,
-                            FtpReply.REPLY_550_REQUESTED_ACTION_NOT_TAKEN,
-                            "STOR.invalid", fileName, file));
-                    return;
-                }
-                fileName = file.getAbsolutePath();
-
-                // get permission
-                if (!file.isWritable()) {
-                    session.write(LocalizedDataTransferFtpReply.translate(session, request, context,
-                            FtpReply.REPLY_550_REQUESTED_ACTION_NOT_TAKEN,
-                            "STOR.permission", fileName, file));
-                    return;
-                }*/
-                // 上面内容可以不要了。
-
                 // 告诉client可以传输
-                // get data connection
-                session.write(
-                        LocalizedFtpReply.translate(session, request, context,
-                                FtpReply.REPLY_150_FILE_STATUS_OKAY, "STOR",
-                                fileName)).awaitUninterruptibly(10000);
-
+                session.write(LocalizedFtpReply.translate(session, request, context,
+                    FtpReply.REPLY_150_FILE_STATUS_OKAY, "STOR",
+                    fileName)).awaitUninterruptibly(10000);
                 // 获取数据传输连接
                 DataConnection dataConnection;
                 try {
@@ -111,54 +80,42 @@ public class FTPServerDemo {
                 } catch (Exception e) {
                     LOG.debug("Exception getting the input data stream", e);
                     session.write(LocalizedDataTransferFtpReply.translate(session, request, context,
-                            FtpReply.REPLY_425_CANT_OPEN_DATA_CONNECTION, "STOR",
-                            fileName, file));
+                        FtpReply.REPLY_425_CANT_OPEN_DATA_CONNECTION,
+                        "STOR", fileName, null));
                     return;
                 }
 
                 // 传输数据
-                // transfer data
                 boolean success = true;
                 long transSz = 0L;
                 try {
                     ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//                    outStream = file.createOutputStream(skipLen);
-//                    transSz = dataConnection.transferFromClient(session.getFtpletSession(), outStream);
                     transSz = dataConnection.transferFromClient(session.getFtpletSession(), baos);
-
-                    // attempt to close the output stream so that errors in
-                    // closing it will return an error to the client (FTPSERVER-119)
-//                    outStream.close();
                     byte[] bytes = baos.toByteArray();
 
-                    LOG.info("File uploaded {}", fileName);
-
+                    LOG.info("File uploaded {} size :{}", fileName, transSz);
                     // 传输完成后，知会统计组件
-                    // notify the statistics component
-                    ServerFtpStatistics ftpStat = (ServerFtpStatistics) context
-                            .getFtpStatistics();
-                    ftpStat.setUpload(session, file, transSz);
-
+                    ServerFtpStatistics ftpStat = (ServerFtpStatistics) context.getFtpStatistics();
+                    ftpStat.setUpload(session, null, transSz);
                 } catch (SocketException ex) {
                     LOG.debug("Socket exception during data transfer", ex);
                     success = false;
                     session.write(LocalizedDataTransferFtpReply.translate(
-                            session, request, context, FtpReply.REPLY_426_CONNECTION_CLOSED_TRANSFER_ABORTED,
-                            "STOR", fileName, file));
+                        session, request, context, FtpReply.REPLY_426_CONNECTION_CLOSED_TRANSFER_ABORTED,
+                        "STOR", fileName, null));
                 } catch (IOException ex) {
                     LOG.debug("IOException during data transfer", ex);
                     success = false;
                     session.write(LocalizedDataTransferFtpReply.translate(
-                            session, request, context, FtpReply.REPLY_551_REQUESTED_ACTION_ABORTED_PAGE_TYPE_UNKNOWN,
-                            "STOR", fileName, file));
+                        session, request, context, FtpReply.REPLY_551_REQUESTED_ACTION_ABORTED_PAGE_TYPE_UNKNOWN,
+                        "STOR", fileName, null));
                 }
 
                 // 告诉 client 已经传输完成且结果正确。
-                // if data transfer ok - send transfer complete message
                 if (success) {
                     session.write(LocalizedDataTransferFtpReply.translate(
-                            session, request, context, FtpReply.REPLY_226_CLOSING_DATA_CONNECTION,
-                            "STOR", fileName, file, transSz));
+                        session, request, context, FtpReply.REPLY_226_CLOSING_DATA_CONNECTION,
+                        "STOR", fileName, null, transSz));
                 }
             } finally {
                 session.resetState();
